6615418dd5b33e9be183e79ada7e1de3b11423e5
Daniil Zhelanov <zdv376@gmail.com>
2025-02-10 16:02:15 +0300

    show favorite quote

--- quotes.js
+++ quotes.js
@@ -4,32 +4,39 @@ const quotes = [
 	{
 		quote: 'The only way to do great work is to love what you do.',
 		author: 'Steve Jobs',
+		isFavorite: false,
 	},
 	{
 		quote:
 			'Success is not the key to happiness. Happiness is the key to success.',
 		author: 'Albert Schweitzer',
+		isFavorite: false,
 	},
 	{
 		quote: 'Don’t watch the clock; do what it does. Keep going.',
 		author: 'Sam Levenson',
+		isFavorite: false,
 	},
 	{
 		quote: 'You miss 100% of the shots you don’t take.',
 		author: 'Wayne Gretzky',
+		isFavorite: false,
 	},
 	{
 		quote: 'Believe you can and you’re halfway there.',
 		author: 'Theodore Roosevelt',
+		isFavorite: false,
 	},
 	{
 		quote:
 			'The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well.',
 		author: 'Ralph Waldo Emerson',
+		isFavorite: false,
 	},
 	{
 		quote: 'Strive not to be a success, but rather to be of value.',
 		author: 'Albert Einstein',
+		isFavorite: false,
 	},
 ]
 
--- script.js
+++ script.js
@@ -4,12 +4,51 @@ import quotes from './quotes.js'
 const quoteElement = document.getElementById('quote')
 const quoteAuthor = document.getElementById('author')
 const genrateBtn = document.getElementById('generate-btn')
+const makeFavoriteBtn = document.getElementById('add-favorite')
+const favoritesContainer = document.getElementById('favorites-container')
+
+let currentQuoteIndex
 
 function generateRandomQuote() {
-	const randomIndex = Math.floor(Math.random() * quotes.length)
-	const randomQuote = quotes[randomIndex]
+	currentQuoteIndex = Math.floor(Math.random() * quotes.length)
+	const randomQuote = quotes[currentQuoteIndex]
 	quoteElement.textContent = randomQuote.quote
 	quoteAuthor.textContent = randomQuote.author
+	if (randomQuote.isFavorite) {
+		makeFavoriteBtn.textContent = 'Remove from favorities'
+	} else {
+		makeFavoriteBtn.textContent = 'Make Favorite'
+	}
+	makeFavoriteBtn.style.display = 'inline-block'
+}
+
+function makeFavorite() {
+	const currentQuote = quotes[currentQuoteIndex]
+	currentQuote.isFavorite = !currentQuote.isFavorite
+	if (makeFavoriteBtn.textContent === 'Make Favorite') {
+		makeFavoriteBtn.textContent = 'Remove from favorities'
+	} else {
+		makeFavoriteBtn.textContent = 'Make Favorite'
+	}
+	if (currentQuote.isFavorite) {
+		const favoriteCard = document.createElement('div')
+		favoriteCard.classList.add('favorite-card')
+		favoriteCard.innerHTML = `
+		<p>${currentQuote.quote}</p>
+		<p class = 'author'>${currentQuote.author}</p>
+		`
+		favoritesContainer.appendChild(favoriteCard)
+	} else {
+		const favoriteCards = document.querySelectorAll('.favorite-card')
+		favoriteCards.forEach(card => {
+			if (card.textContent.includes(currentQuote.quote)) {
+				card.remove()
+			}
+		})
+	}
 }
 
 genrateBtn.addEventListener('click', generateRandomQuote)
+makeFavoriteBtn.addEventListener('click', makeFavorite)
+
+generateRandomQuote()
